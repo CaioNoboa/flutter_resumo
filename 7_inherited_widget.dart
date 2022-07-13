4bee2a01679c0423e9fafd8b70618837b4127b1f
CaioNoboa <caiosalvador@hotmail.com>
2022-07-13 09:50:23 -0300

    Inherited Widget

---
+++ 7_inherited_widget.dart
@@ -0,0 +1,17 @@
+/*
+  Situação:
+    Precisamos passar uma informação de um componente para outro.
+  
+  Opções:
+    Comunicação direta;
+    Comunicação indireta;
+      Para ver mais sobre comunicação direta e indireta, acesse https://github.com/CaioNoboa/comunicacao-entre-widgets-Flutter
+    
+    Contudo, quando componentes estão mais distantes, com grande número de componentes, fica mais extensivo utilizar comunicação direta/indireta. Dessa forma, uma possível solução é utilizar inherited widget (wisget herdado).
+
+    Inherited Widget
+      O inherited widget será um componente dentro da árvore de componentes. A partir dele, conseguimos ter uma comunicação direta a partir de vários componentes.
+      Funciona a partir de contextos, da árvore de contextos, a partir disso ocorrerá a comunicação.
+
+      Acessaremos a partir do 'provider', método estático 'of'.
+*/
