b18b6ec8a6c01dfb043a39e45e019e2375b40e0d
CaioNoboa <caiosalvador@hotmail.com>
2022-07-14 15:20:36 -0300

    inherited_widget

--- 7_inherited_widget.dart
+++ 7_inherited_widget.dart
@@ -20,11 +20,14 @@
       Acessamos o inherited widget a partir da árvore de componentes sem precisar passar informações de um componente para outro.
 
       Provider
-        Inserido em algum componente da árvore.
+        Inserido em algum componente da árvore. Envolve o Inherited Widget facilitando seu uso.
+
         Será um container, um widget. A partir disso, todos os componentes filhos conseguirão acessar informações desse provider diretamente, sem necessidade de ferramentas de comunicação direta/indireta.
         Acessaremos o provider a partir de 'of(context)'.
 
         Quando o estado muda, o método buil é chamado novamente e a interface é atualizada. Mas em componente stateless é preciso passar informação via construtor para que haja atualização. Para que a interface seja atualizada utilizando o of(context), eventualmente será necessário statefull widget.
 
         O provider não precisa ser definido na raiz da árvore, pode ser definido em um ramo da árvore. Mas ao fazer isso, esses dados não conseguirão ser acessados em componentes de outros ramos.
+
+        Listening mechanism -> ChangeNotifier (padrão Observer).
 */
