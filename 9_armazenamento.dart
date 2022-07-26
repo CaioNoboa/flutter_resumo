87c81d0c9022b799a82bbdccac8d9585d42af7a2
Caio Salvador Noboa <caiosalvador@hotmail.com>
2022-07-26 10:44:02 -0300

    armazenamento-de-dados

---
+++ 9_armazenamento.dart
@@ -0,0 +1,15 @@
+/*
+  Formas de armazenamento:
+
+    Armazenamento local (no dispositivo)
+      Dados disponíveis apenas na aplicação, ou seja, apenas para aquele usuário.
+      Dados podem ser persistentes. Podemos usar banco de dados dentro da aplicação.
+      Pode trabalhar offline.
+
+    Armazenamento remoto (Web Server)
+      Dados disponíveis para múltiplos usuários.
+      Dados persistentes.
+      Precisa de internet, estar online.
+
+    Podemos unir armazenamento local e remoto
+*/
